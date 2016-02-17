1d3b8905b6d88b14160a81572eeabb01f9ed7476
nikodraca <nikola.draca@live.ca>
2016-02-16 22:29:12 -0500

    troubles

--- content.js
+++ content.js
@@ -1,20 +1,33 @@
 var elements = document.links;
 
 function info(json) {
-    var rv = null;
+
+    rv = null;
 
     $.getJSON(json ,function(data) {
-        $.each(data.query.pages, function(x, item)
-            {
-                debugger;
-                rv = item.extract;
-                console.log(rv);
-                return false;
-            });
+        rv = data.query.pages;
+        // need to access next pageID
+
+
+        // JSON example:
+        // {
+        //   "batchcomplete": "",
+        //   "query": {
+        //     "pages": {
+        //       "18737268": {
+        //         "pageid": 18737268,
+        //         "ns": 0,
+        //         "title": "SolarCity",
+        //         "extract": "SolarCity is an American provider of energy services, headquartered in San Mateo, California."
+        //       }
+        //     }
+        //   }
+        // }
 
     });
 
     return rv;
+
 }
 
 
@@ -23,9 +36,8 @@ for (var i=0; i<elements.length; i++) {
     var str = elements[i].href.split("wiki/")[1];
     var json = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+str+"&exsentences=1";
 
-    // console.log(json.query);
+    console.log(json);
 
-    elements[i].title = info(json);
 
 
 }
