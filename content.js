846f16ae15ed0e45f0f8c584677af389dc263ac6
nikodraca <nikola.draca@live.ca>
2016-02-18 10:43:26 -0500

    wiki match + error handles

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 var elements = document.links;
+console.log("active");
 
 function info(json) {
 
@@ -22,10 +23,17 @@ function info(json) {
 
 for (var i=0; i<elements.length; i++) {
     if ((elements[i].href).indexOf("wiki") > -1) {
+
         var str = elements[i].href.split("wiki/")[1];
-        var strx = str.replace(" ","_");
+        var strx = str.replace(" ","_").replace("?","%3F");
         var json = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+strx+"&exsentences=1&formatversion=2";
-        elements[i].title = info(json);     
+        
+        if (info(json) == "This is a redirect from a topic that does not have its own page to a section of a page on the subject."){
+          elements[i].title = "Not available due to redirect."
+        }
+        else{
+          elements[i].title = info(json);   
+        }  
     }
 
 }
