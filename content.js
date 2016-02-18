684dd6f6324ea45618f3f0b1ffcea911f8a8a0bd
nikodraca <nikola.draca@live.ca>
2016-02-18 01:16:39 -0500

    omit async for testing + ajax json request

--- content.js
+++ content.js
@@ -4,40 +4,28 @@ function info(json) {
 
     rv = null;
 
-    $.getJSON(json ,function(data) {
-        rv = data.query.pages;
-        // need to access next pageID
+    $.ajax({
+      url: json,
+      dataType: 'json',
+      async: false,
 
+      success: function(data) {
+        rv = data.query.pages[0].extract;
 
-        // JSON example:
-        // {
-        //   "batchcomplete": "",
-        //   "query": {
-        //     "pages": {
-        //       "18737268": {
-        //         "pageid": 18737268,
-        //         "ns": 0,
-        //         "title": "SolarCity",
-        //         "extract": "SolarCity is an American provider of energy services, headquartered in San Mateo, California."
-        //       }
-        //     }
-        //   }
-        // }
-
-    });
+              }
+        });
 
     return rv;
 
 }
 
 
-
 for (var i=0; i<elements.length; i++) {
-    var str = elements[i].href.split("wiki/")[1];
-    var json = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+str+"&exsentences=1";
-
-    console.log(json);
-
-
+    if ((elements[i].href).indexOf("wiki") > -1) {
+        var str = elements[i].href.split("wiki/")[1];
+        var strx = str.replace(" ","_");
+        var json = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+strx+"&exsentences=1&formatversion=2";
+        elements[i].title = info(json);     
+    }
 
 }
