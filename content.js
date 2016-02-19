e8ba2ef72f6031026f996cae627dd1fd97a0dce2
nikodraca <nikola.draca@live.ca>
2016-02-18 22:20:07 -0500

    fix regex

--- content.js
+++ content.js
@@ -1,14 +1,14 @@
 (function () {
   var elements = document.links;
   Array.prototype.forEach.call(elements, function (elem) {
-    var urlParse = elem.href.match(/https\:\/\/en\.wikipedia\.org\/wiki\/([A-z0-9_\-]+)/);
+  var urlParse = elem.href.match(/https\:\/\/en\.wikipedia\.org\/wiki\/([A-z0-9_\-\(\)]+)/);
     if(urlParse && urlParse.length){
       var jsonURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+urlParse[1]+"&exsentences=1&formatversion=2";
       $.ajax({
         url: jsonURL,
         dataType: 'json',
         success: function(data) {
-          var rv = data.query.pages[0].extract; // Return 1st sentence of article
+          var rv = data.query.pages[0].extract;
           elem.title = rv;
         },
         fail: function (failData) {
