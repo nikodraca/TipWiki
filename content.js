80a945c8170d7ce0e2b22841f4d3877b5c93cefa
Angus McLean <Angus McLean>
2016-02-18 19:34:10 -0500

    reformates content.js to execute calls asynchronously

--- content.js
+++ content.js
@@ -1,44 +1,22 @@
-var elements = document.links; // Grab all links on page (still an issue with not grabbing all links)
-
-function info(json) {
-
-    rv = null;
-
-    // Turn link into JSON object
-    $.ajax({
-      url: json,
-      dataType: 'json',
-      async: false, // VERY BAD, this is why it is slow, will find solution 
-
-      success: function(data) {
-        rv = data.query.pages[0].extract; // Return 1st sentence of article
-        
-              }
-        });
-
-    return rv;
-
-}
-
-
-for (var i=0; i<elements.length; i++) {
-    if ((elements[i].href).indexOf("wiki") > -1) { // Look for links to lead to articles
-
-        var str = elements[i].href.split("wiki/")[1]; // Extract parameter for API call
-        var strx = str.replace(" ","_").replace("?","%3F"); // Replace certain characters to make successful API Call
-        var json = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+strx+"&exsentences=1&formatversion=2"; // Wikipedia endpoint
-
-
-        if (info(json) == "This is a redirect from a topic that does not have its own page to a section of a page on the subject.")
-        {
-          elements[i].title = "Not available due to redirect." // There are still lots of other redirects not being caught, will fix
+(function () {
+  var elements = document.links;
+  Array.prototype.forEach.call(elements, function (elem) {
+    var urlParse = elem.href.match(/https\:\/\/en\.wikipedia\.org\/wiki\/([A-z0-9_\-]+)/);
+    if(urlParse && urlParse.length){
+      var jsonURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="+urlParse[1]+"&exsentences=1&formatversion=2";
+      $.ajax({
+        url: jsonURL,
+        dataType: 'json',
+        success: function(data) {
+          var rv = data.query.pages[0].extract; // Return 1st sentence of article
+          elem.title = rv;
+        },
+        fail: function (failData) {
+          console.error(failData);
+          elem.title = "Not Available";
         }
-
-        else
-        {
-          elements[i].title = info(json); // If no errors, change title attribute of link (change tooltip content)   
-        }  
-
+      });
     }
+  })
+})();
 
-}
